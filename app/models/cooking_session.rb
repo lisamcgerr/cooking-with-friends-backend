24431d68be198abcfc39410ece157cb05d344303
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-03-25 18:41:00 -0500

    model changes to cooking session

--- app/models/cooking_session.rb
+++ app/models/cooking_session.rb
@@ -2,4 +2,7 @@ class CookingSession < ApplicationRecord
   belongs_to :recipe
   has_many :user_sessions
   has_many :users, through: :user_sessions
+
+  validates :title, uniqueness: true
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
     has_many :recipes, through: :cooking_sessions
     has_many :comments
 
-    # validates :username, uniqueness: true
-    # validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, presence: true
     # validates :password, length: { in: 8..15 }
-    # validates :email, uniqueness: true
+    validates :email, uniqueness: true
     validates :email, presence: true
 
     has_secure_password
