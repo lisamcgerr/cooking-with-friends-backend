2c631c09f064e81e81adbde315c16ac5fb89c935
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-03-28 19:58:19 -0500

    adding serializer edits to comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :post, :created_at
+  attributes :id, :user_id, :recipe_id, :post, :created_at
   
   belongs_to :user
   belongs_to :recipe
